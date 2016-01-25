7c221c4577fc8d1fd38993d1961bdf4842e8e6bb
Jason Shi <shuwu.shi@gmail.com>
2016-01-25 16:57:17 +0800

    knockout testing

--- src/main/webapp/js/view-model.js
+++ src/main/webapp/js/view-model.js
@@ -1,5 +1,7 @@
 var viewModel = {
 	chartTypes : ko.observableArray([ 'Pie', 'Bar' ]),
+	selectedChartType : ko.observable(),
+	
 	dimensions : ko.observableArray([ 'P_Name', 'P_Region', 'P_Year' ]),
 	metrics : ko.observableArray([ 'P_Price', 'P_Score' ]),
 	width : ko.observable(600),
